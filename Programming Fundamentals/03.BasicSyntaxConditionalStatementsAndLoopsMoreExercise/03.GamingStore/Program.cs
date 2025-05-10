9d8c0bb95824eb05ca52b71b20eea2931e66675f
Hristiyan Yordanov <hristianjordanov08@gmail.com>
2025-05-10 22:44:07 +0300

    Add additional tasks from first exercise

---
+++ Programming Fundamentals/03.BasicSyntaxConditionalStatementsAndLoopsMoreExercise/03.GamingStore/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _03.GamingStore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double balance = double.Parse(Console.ReadLine());
+        double currentBalance = balance;
+        string input = "";
+        double gameBalance  = 0;
+        string gameName = "";
+        while ((input = Console.ReadLine()) != "Game Time")
+        {
+            
+            switch (input)
+            {
+                case "OutFall 4":
+                    gameBalance = 39.99;
+                    gameName = "OutFall 4";
+                    break;
+                case "CS: OG":
+                    gameBalance = 15.99;
+                    gameName = "CS: OG";
+                    break;
+                case "Zplinter Zell":
+                    gameBalance = 19.99;
+                    gameName = "Zplinter Zell";
+                    break;
+                case "Honored 2":
+                    gameBalance = 59.99;
+                    gameName = "Honored 2";
+                    break;
+                case "RoverWatch":
+                    gameBalance = 29.99;
+                    gameName = "RoverWatch";
+                    break;
+                case "RoverWatch Origins Edition":
+                    gameBalance = 39.99;
+                    gameName = "RoverWatch Origins Edition";
+                    break;
+                default:
+                    Console.WriteLine("Not Found");
+                    continue;
+            }
+
+            if (currentBalance >= gameBalance)
+            {
+                currentBalance -= gameBalance;
+                Console.WriteLine($"Bought {gameName}");
+            }
+            else
+            {
+                Console.WriteLine("Too Expensive");
+            }
+            if (currentBalance == 0.00)
+            {
+                Console.WriteLine("Out of money!");
+                return;
+            }
+        }
+
+        Console.WriteLine($"Total spent: ${balance-currentBalance:F2}. Remaining: ${currentBalance:F2}");
+    }
+}
---
+++ Programming Fundamentals/03.BasicSyntaxConditionalStatementsAndLoopsMoreExercise/05.Messages/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _05.Messages;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int countOfWords = int.Parse(Console.ReadLine());
+        string word = "";
+        for (int i = 1; i <= countOfWords; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int countOfDigits = 0;
+            int mainDigit = number % 10;
+            while (number > 0)
+            {
+                countOfDigits++;
+                number /= 10;
+            }
+
+            int offset = (mainDigit - 2) * 3;
+            if (mainDigit == 8 || mainDigit == 9)
+            {
+                offset++;
+            }
+
+            int letterIndex = offset + countOfDigits - 1 + 97;
+            if (letterIndex == 90)
+            {
+                word += " ";
+            }
+            else
+            {
+                word += (char)letterIndex;
+            }
+        }
+
+        Console.WriteLine(word);
+    }
+}
